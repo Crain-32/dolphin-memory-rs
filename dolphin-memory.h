b9f40f314b99ab7d3bcaefcf3a022282ea744f1c
Crain-32 <petereastham@gmail.com>
2022-12-04 11:41:43 -0700

    Changes

---
+++ dolphin-memory.h
@@ -0,0 +1,15 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <ostream>
+#include <new>
+
+constexpr static const uintptr_t MEM1_STRIP_START = 1073741823;
+
+constexpr static const uintptr_t MEM1_START = 268435456;
+
+constexpr static const uintptr_t MEM1_END = 2172649472;
+
+constexpr static const uintptr_t MEM1_SIZE = 33554432;
+
+constexpr static const uintptr_t MEM2_SIZE = 67108864;
--- src/lib.rs
+++ src/lib.rs
@@ -87,17 +87,17 @@ impl Dolphin {
     pub fn new() -> Result<Self, ProcessError> {
         let handle = match get_pid(vec!["Dolphin.exe", "DolphinQt1.exe", "DolphinWx.exe"]) {
             Some(h) => h
-                .try_into_process_handle()
-                .map_err(|e| ProcessError::UnknownError(e))?,
+            .try_into_process_handle()
+            .map_err(|e| ProcessError::UnknownError(e))?,
             None => return Err(ProcessError::DolphinNotFound),
         };
-
+        
         let ram = ram_info(handle)?;
         let handle = handle.set_arch(process_memory::Architecture::Arch32Bit);
-
+        
         Ok(Dolphin { handle, ram })
     }
-
+    
     // is_emulation_running queries ram info to determine if the emulator is still running.
     pub fn is_emulation_running(&self) -> bool {
         match ram_info(self.handle) {
@@ -105,7 +105,7 @@ impl Dolphin {
             Err(_) => false,
         }
     }
-
+    
     // read takes a size, starting address and an optional list of pointer offsets,
     // following those addresses until it hits the underyling data.
     //
@@ -117,33 +117,26 @@ impl Dolphin {
     // every single time it's run, but in all likelihood the addresses will not
     // change that frequently. It would be a good idea to introduce a cache layer here
     // which caches the output address using a hash of the input address + offsets.
-    pub fn read(
+    #[no_mangle]
+    pub extern "C" fn read(
         &self,
         size: usize,
         starting_address: usize,
-        pointer_offsets: Option<&[usize]>,
     ) -> io::Result<Vec<u8>> {
         // TODO: this should realistically be able to handle picking mem_1 or mem_2,
         // but we'll just stick to mem_1 for now.
         let starting_address = starting_address & MEM1_STRIP_START;
         let mut buffer = vec![0_u8; size];
-
-        if let Some(offsets) = pointer_offsets {
-            let addr = self.resolve_pointer_address(starting_address, offsets)?;
-            self.handle.copy_address(addr, &mut buffer)?;
-
-            return Ok(buffer);
-        }
-
+        
         let address = self
-            .handle
-            .get_offset(&[self.ram.mem_1 + starting_address])?;
-
+        .handle
+        .get_offset(&[self.ram.mem_1 + starting_address])?;
+        
         self.handle.copy_address(address, &mut buffer)?;
-
+        
         Ok(buffer)
     }
-
+    
     // write a buffer of bytes to the given address or pointer of address.
     pub fn write(
         &self,
@@ -154,16 +147,16 @@ impl Dolphin {
         if let Some(offsets) = pointer_offsets {
             let addr = self.resolve_pointer_address(starting_address, offsets)?;
             self.handle.put_address(addr, buf)?;
-
+            
             return Ok(());
         }
-
+        
         let starting_address = self.ram.mem_1 + (starting_address & MEM1_STRIP_START);
         self.handle.put_address(starting_address, buf)?;
-
+        
         Ok(())
     }
-
+    
     // write_u8 wraps write and provides a simple interface for writing a u8 to dolphin memory
     pub fn write_u8(
         &self,
@@ -173,7 +166,7 @@ impl Dolphin {
     ) -> io::Result<()> {
         self.write(&[n], starting_address, pointer_offsets)
     }
-
+    
     // write_u16 wraps write and provides a simple interface for writing a u16 to dolphin memory
     pub fn write_u16(
         &self,
@@ -183,7 +176,7 @@ impl Dolphin {
     ) -> io::Result<()> {
         self.write(&n.to_be_bytes(), starting_address, pointer_offsets)
     }
-
+    
     // write_u32 wraps write and provides a simple interface for writing a u32 to dolphin memory
     pub fn write_u32(
         &self,
@@ -193,7 +186,7 @@ impl Dolphin {
     ) -> io::Result<()> {
         self.write(&n.to_be_bytes(), starting_address, pointer_offsets)
     }
-
+    
     // write_i8 wraps write and provides a simple interface for writing a i8 to dolphin memory
     pub fn write_i8(
         &self,
@@ -203,7 +196,7 @@ impl Dolphin {
     ) -> io::Result<()> {
         self.write(&n.to_be_bytes(), starting_address, pointer_offsets)
     }
-
+    
     // write_i16 wraps write and provides a simple interface for writing a i16 to dolphin memory
     pub fn write_i16(
         &self,
@@ -213,7 +206,7 @@ impl Dolphin {
     ) -> io::Result<()> {
         self.write(&n.to_be_bytes(), starting_address, pointer_offsets)
     }
-
+    
     // write_i32 wraps write and provides a simple interface for writing a i32 to dolphin memory
     pub fn write_i32(
         &self,
@@ -223,7 +216,7 @@ impl Dolphin {
     ) -> io::Result<()> {
         self.write(&n.to_be_bytes(), starting_address, pointer_offsets)
     }
-
+    
     // write_f32 wraps write and provides a simple interface for writing an f32 to dolphin memory
     pub fn write_f32(
         &self,
@@ -233,91 +226,84 @@ impl Dolphin {
     ) -> io::Result<()> {
         self.write(&f.to_be_bytes(), starting_address, pointer_offsets)
     }
-
+    
     // read_u8 wraps read to provide a convenient cast to an u8.
     pub fn read_u8(
         &self,
-        starting_address: usize,
-        pointer_offsets: Option<&[usize]>,
+        starting_address: usize
     ) -> io::Result<u8> {
-        let buf = self.read(mem::size_of::<u8>(), starting_address, pointer_offsets)?;
+        let buf = self.read(mem::size_of::<u8>(), starting_address)?;
         let n = std::io::Cursor::new(buf).read_u8()?;
-
+        
         Ok(n)
     }
-
+    
     // read_u16 wraps read to provide a convenient cast to an u16.
     pub fn read_u16(
         &self,
         starting_address: usize,
-        pointer_offsets: Option<&[usize]>,
     ) -> io::Result<u16> {
-        let buf = self.read(mem::size_of::<u16>(), starting_address, pointer_offsets)?;
+        let buf = self.read(mem::size_of::<u16>(), starting_address)?;
         let n = std::io::Cursor::new(buf).read_u16::<BigEndian>()?;
-
+        
         Ok(n)
     }
-
+    
     // read_u32 wraps read to provide a convenient cast to an u32.
     pub fn read_u32(
         &self,
         starting_address: usize,
-        pointer_offsets: Option<&[usize]>,
     ) -> io::Result<u32> {
-        let buf = self.read(mem::size_of::<u32>(), starting_address, pointer_offsets)?;
+        let buf = self.read(mem::size_of::<u32>(), starting_address)?;
         let n = std::io::Cursor::new(buf).read_u32::<BigEndian>()?;
-
+        
         Ok(n)
     }
-
+    
     // read_i8 wraps read to provide a convenient cast to an i8.
     pub fn read_i8(
         &self,
         starting_address: usize,
-        pointer_offsets: Option<&[usize]>,
     ) -> io::Result<i8> {
-        let buf = self.read(mem::size_of::<i8>(), starting_address, pointer_offsets)?;
+        let buf = self.read(mem::size_of::<i8>(), starting_address)?;
         let n = std::io::Cursor::new(buf).read_i8()?;
-
+        
         Ok(n)
     }
-
+    
     // read_i16 wraps read to provide a convenient cast to an i16.
     pub fn read_i16(
         &self,
         starting_address: usize,
-        pointer_offsets: Option<&[usize]>,
     ) -> io::Result<i16> {
-        let buf = self.read(mem::size_of::<i16>(), starting_address, pointer_offsets)?;
+        let buf = self.read(mem::size_of::<i16>(), starting_address)?;
         let n = std::io::Cursor::new(buf).read_i16::<BigEndian>()?;
-
+        
         Ok(n)
     }
-
+    
     // read_i32 wraps read to provide a convenient cast to an i32.
     pub fn read_i32(
         &self,
         starting_address: usize,
-        pointer_offsets: Option<&[usize]>,
     ) -> io::Result<i32> {
-        let buf = self.read(mem::size_of::<i32>(), starting_address, pointer_offsets)?;
+        let buf = self.read(mem::size_of::<i32>(), starting_address)?;
         let n = std::io::Cursor::new(buf).read_i32::<BigEndian>()?;
-
+        
         Ok(n)
     }
-
+    
     // read_f32 wraps read to provide a convenient cast to an f32.
     pub fn read_f32(
         &self,
         starting_address: usize,
-        pointer_offsets: Option<&[usize]>,
     ) -> io::Result<f32> {
-        let buf = self.read(mem::size_of::<f32>(), starting_address, pointer_offsets)?;
+        let buf = self.read(mem::size_of::<f32>(), starting_address)?;
         let f = std::io::Cursor::new(buf).read_f32::<BigEndian>()?;
-
+        
         Ok(f)
     }
-
+    
     // read_string provides a convenient read and cast to a String.
     // Note that strings are expected to be utf8 and the length will account
     // for the number of bytes to read.
@@ -325,14 +311,13 @@ impl Dolphin {
         &self,
         length: usize,
         starting_address: usize,
-        pointer_offsets: Option<&[usize]>,
     ) -> io::Result<String> {
-        let buf = self.read(length, starting_address, pointer_offsets)?;
+        let buf = self.read(length, starting_address)?;
         let string = String::from_utf8(buf).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-
+        
         Ok(string)
     }
-
+    
     // resolve_pointer_address follows a series of pointer offsets until
     // it reachines the last pointer, in which case that address is returned.
     //
@@ -346,38 +331,38 @@ impl Dolphin {
         // TODO: this should realistically be able to handle picking mem_1 or mem_2,
         // but we'll just stick to mem_1 for now.
         let starting_address = starting_address & MEM1_STRIP_START;
-
+        
         // read the starting address to get the initial pointer.
         // we could have multiple pointer chains to follow, but we know the starting
         // address is where we want to look first to kick ourselves off on the right foot.
         let mut ptr_buffer = vec![0_u8; std::mem::size_of::<u32>()];
         self.handle
-            .copy_address(self.ram.mem_1 + starting_address, &mut ptr_buffer)?;
-
+        .copy_address(self.ram.mem_1 + starting_address, &mut ptr_buffer)?;
+        
         let mut current_ptr: usize = io::Cursor::new(ptr_buffer)
-            .read_u32::<BigEndian>()?
-            .try_into()
-            .map_err(|e: TryFromIntError| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
-
+        .read_u32::<BigEndian>()?
+        .try_into()
+        .map_err(|e: TryFromIntError| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+        
         if current_ptr == 0 {
             return Err(io::Error::new(
                 io::ErrorKind::InvalidData,
                 "null pointer address",
             ));
         }
-
+        
         let mut ptr_addr: usize = 0;
         for (index, offset) in offsets.iter().enumerate() {
             // We'll need a new ptr_buffer for each pointer
             let mut ptr_buffer = vec![0_u8; std::mem::size_of::<u32>()];
-
+            
             // Copy and update the current address for each iteration,
             // as this will be our starting point on subsequent loops.
             // This also conveniently handles dropping the `8` a the start
             // of all MEM1 addresses.
             // TODO: Update this to better handle MEM2.
             let addr = (current_ptr & MEM1_STRIP_START) + offset;
-
+            
             // the last iteration we've likely reached the value we're looking
             // for, so lets just copy that and break out.
             if index == offsets.len() - 1 {
@@ -387,20 +372,20 @@ impl Dolphin {
                         "null pointer address",
                     ));
                 }
-
+                
                 ptr_addr = self.ram.mem_1 + addr;
                 break;
             }
-
+            
             self.handle
-                .copy_address(self.ram.mem_1 + addr, &mut ptr_buffer)?;
+            .copy_address(self.ram.mem_1 + addr, &mut ptr_buffer)?;
             current_ptr = io::Cursor::new(ptr_buffer)
-                .read_u32::<BigEndian>()?
-                .try_into()
-                .map_err(|e: TryFromIntError| {
-                    io::Error::new(io::ErrorKind::Other, e.to_string())
-                })?;
-
+            .read_u32::<BigEndian>()?
+            .try_into()
+            .map_err(|e: TryFromIntError| {
+                io::Error::new(io::ErrorKind::Other, e.to_string())
+            })?;
+            
             if current_ptr == 0 {
                 return Err(io::Error::new(
                     io::ErrorKind::InvalidData,
@@ -408,14 +393,14 @@ impl Dolphin {
                 ));
             }
         }
-
+        
         if ptr_addr == 0 {
             return Err(io::Error::new(
                 io::ErrorKind::InvalidData,
                 "null pointer address",
             ));
         }
-
+        
         Ok(ptr_addr)
     }
 }
@@ -426,11 +411,11 @@ fn get_pid(process_names: Vec<&str>) -> Option<process_memory::Pid> {
         use std::ffi::CStr;
         unsafe {
             CStr::from_ptr(bytes.as_ptr())
-                .to_string_lossy()
-                .into_owned()
+            .to_string_lossy()
+            .into_owned()
         }
     }
-
+    
     let mut entry = winapi::um::tlhelp32::PROCESSENTRY32 {
         dwSize: std::mem::size_of::<winapi::um::tlhelp32::PROCESSENTRY32>() as u32,
         szExeFile: [0; winapi::shared::minwindef::MAX_PATH],
@@ -443,24 +428,24 @@ fn get_pid(process_names: Vec<&str>) -> Option<process_memory::Pid> {
         pcPriClassBase: 0,
         dwFlags: 0,
     };
-
+    
     let snapshot: winapi::um::winnt::HANDLE;
     snapshot = unsafe {
         winapi::um::tlhelp32::CreateToolhelp32Snapshot(winapi::um::tlhelp32::TH32CS_SNAPPROCESS, 0)
     };
-
+    
     if unsafe { winapi::um::tlhelp32::Process32First(snapshot, &mut entry) }
-        == winapi::shared::minwindef::TRUE
+    == winapi::shared::minwindef::TRUE
     {
         while unsafe { winapi::um::tlhelp32::Process32Next(snapshot, &mut entry) }
-            == winapi::shared::minwindef::TRUE
+        == winapi::shared::minwindef::TRUE
         {
             if process_names.contains(&utf8_to_string(&entry.szExeFile).as_str()) {
                 return Some(entry.th32ProcessID);
             }
         }
     }
-
+    
     None
 }
 
@@ -468,7 +453,7 @@ fn get_pid(process_names: Vec<&str>) -> Option<process_memory::Pid> {
 fn ram_info(process: ProcessHandle) -> Result<EmuRAMAddresses, ProcessError> {
     let mut mem1: Option<usize> = None;
     let mut mem2: Option<usize> = None;
-
+    
     let mut p = ptr::null_mut();
     let mut info = winnt::MEMORY_BASIC_INFORMATION::default();
     loop {
@@ -484,11 +469,11 @@ fn ram_info(process: ProcessHandle) -> Result<EmuRAMAddresses, ProcessError> {
         if size != mem::size_of::<winnt::MEMORY_BASIC_INFORMATION>() {
             break;
         }
-
+        
         // check region size so that we know it's mem2
         if info.RegionSize == MEM2_SIZE {
             let region_base_address = info.BaseAddress as usize;
-
+            
             if let Some(region) = mem1 {
                 if region_base_address > region + MEM1_START {
                     // in some cases MEM2 could actually be before MEM1. Once we find
@@ -497,7 +482,7 @@ fn ram_info(process: ProcessHandle) -> Result<EmuRAMAddresses, ProcessError> {
                     break;
                 }
             }
-
+            
             // View the comment for MEM1
             let mut ws_info = psapi::PSAPI_WORKING_SET_EX_INFORMATION {
                 VirtualAddress: info.BaseAddress,
@@ -508,8 +493,8 @@ fn ram_info(process: ProcessHandle) -> Result<EmuRAMAddresses, ProcessError> {
                     process.0,
                     &mut ws_info as *mut _ as *mut ffi::c_void,
                     mem::size_of::<psapi::PSAPI_WORKING_SET_EX_INFORMATION>()
-                        .try_into()
-                        .unwrap(),
+                    .try_into()
+                    .unwrap(),
                 ) }{
                     0 => Err(io::Error::last_os_error()),
                     _ => Ok(()),
@@ -539,8 +524,8 @@ fn ram_info(process: ProcessHandle) -> Result<EmuRAMAddresses, ProcessError> {
                     process.0,
                     &mut ws_info as *mut _ as *mut ffi::c_void,
                     mem::size_of::<psapi::PSAPI_WORKING_SET_EX_INFORMATION>()
-                        .try_into()
-                        .unwrap(),
+                    .try_into()
+                    .unwrap(),
                 ) }{
                     0 => Err(io::Error::last_os_error()),
                     _ => Ok(()),
@@ -557,19 +542,19 @@ fn ram_info(process: ProcessHandle) -> Result<EmuRAMAddresses, ProcessError> {
                 }
             }
         }
-
+        
         if mem1.is_some() && mem2.is_some() {
             break;
         }
-
+        
         // iter through region size
         unsafe { p = p.add(info.RegionSize) };
     }
-
+    
     if mem1.is_none() {
         return Err(ProcessError::EmulationNotRunning);
     }
-
+    
     Ok(EmuRAMAddresses {
         mem_1: mem1.unwrap_or_default(),
         mem_2: mem2.unwrap_or_default(),
